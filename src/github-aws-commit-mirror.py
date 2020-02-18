01ee91d4a2fedf36010a5a654b32bf3fd02ce25b
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-18 14:25:31 +0100

    Clone test

--- src/github-aws-commit-mirror.py
+++ src/github-aws-commit-mirror.py
@@ -10,12 +10,15 @@ def is_archived(repo):
   repo.archived
 
 
+def clone(repo_name):
+  os.system('git clone https://github.com/rribeiro1/{}.git'.format(repo_name))
+
+
 for repo in g.get_user().get_repos():
   if is_archived(repo):
     print('Skipping repository {}, it is archived on github'.format(repo.name))
   else:
     if repo.name in ['bible-vue', 'bible-edge']:
-      print('Mirroring repository {}'.format(repo.name))
-      os.system('git clone https://github.com/rribeiro1/{}.git'.format(repo.name))
-
-
+      print('Clonning repository {} locally'.format(repo.name))
+      clone(repo.name)
+      
