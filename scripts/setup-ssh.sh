1be87d2051410913e63d0ea24ebabe0d972ef897
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-20 00:02:58 +0100

    wip

--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -11,6 +11,8 @@ cat /home/circleci/.ssh/id_rsa_ffeb190510d862a23307b06f1dd70fcf
  User APKAYMLLNMYWO5TS2QMO
  IdentityFile /home/circleci/.ssh/id_rsa" >> /home/circleci/.ssh/config
 
+ls -ltra ~/.ssh
+
 # echo "After changes"
 # cat /home/circleci/.ssh/config
 
