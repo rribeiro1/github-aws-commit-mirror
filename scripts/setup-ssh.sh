7f41c2f370081e81983c0054914d75daeb53cdd3
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-19 22:44:35 +0100

    wip

--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -7,4 +7,6 @@ IdentityFile /home/circleci/.ssh/id_rsa_ffeb190510d862a23307b06f1dd70fcf" >> /ho
 echo "Known hosts"
 cat /home/circleci/.ssh/known_hosts
 
+ssh-keyscan -H git-codecommit.us-east-2.amazonaws.com >> ~/.ssh/known_hosts
+
 ssh -v APKAYMLLNMYWO5TS2QMO@git-codecommit.us-east-2.amazonaws.com
