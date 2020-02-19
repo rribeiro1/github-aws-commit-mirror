01689236ea3cd6c23eeaf40ffb4027ff64825f4c
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-19 22:40:30 +0100

    wip

--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -4,4 +4,7 @@ echo "Host !github.com *
 User APKAYMLLNMYWO5TS2QMO
 IdentityFile /home/circleci/.ssh/id_rsa_ffeb190510d862a23307b06f1dd70fcf" >> /home/circleci/.ssh/config
 
+echo "Known hosts"
+cat /home/circleci/.ssh/config/known_hosts
+
 ssh -v APKAYMLLNMYWO5TS2QMO@git-codecommit.us-east-2.amazonaws.com
