dbfc420488896be0770d486fa885808c8afd4572
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-19 22:17:09 +0100

    wip

--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -1,5 +1,5 @@
 echo "
-Host !github.com *
+Host git-codecommit.*.amazonaws.com
   User AKIAYMLLNMYWP6P7RUB3
   IdentityFile /home/circleci/.ssh/id_rsa_ffeb190510d862a23307b06f1dd70fcf
   " >> ~/.ssh/config
