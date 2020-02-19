f1e11287a772b4308a329a8bf626fa2169191579
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-19 22:42:02 +0100

    wip

--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -5,6 +5,6 @@ User APKAYMLLNMYWO5TS2QMO
 IdentityFile /home/circleci/.ssh/id_rsa_ffeb190510d862a23307b06f1dd70fcf" >> /home/circleci/.ssh/config
 
 echo "Known hosts"
-cat /home/circleci/.ssh/config/known_hosts
+cat /home/circleci/.ssh/known_hosts
 
 ssh -v APKAYMLLNMYWO5TS2QMO@git-codecommit.us-east-2.amazonaws.com
