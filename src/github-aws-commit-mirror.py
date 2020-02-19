b4293a7bcc3490330b6828a71a6e20caad40c8d2
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-19 10:40:59 +0100

    Improve pipeline

--- src/github-aws-commit-mirror.py
+++ src/github-aws-commit-mirror.py
@@ -1,24 +1,52 @@
 from github import Github
+import boto3
 import os
 
 GITHUB_API_TOKEN = os.getenv('GITHUB_API_TOKEN')
 
-g = Github(GITHUB_API_TOKEN)
+github_client = Github(GITHUB_API_TOKEN)
+codecommit_client = boto3.client(
+    'codecommit',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
+)
 
 
-def is_archived(repo):
-  repo.archived
+def clone_repo(repo_name):
+    os.system('git clone --mirror https://github.com/rribeiro1/{}.git {}'.format(repo_name, repo_name))
 
 
-def clone(repo_name):
-  os.system('git clone https://github.com/rribeiro1/{}.git'.format(repo_name))
+def delete_repo_local(repo_name):
+    os.system('rm -Rf {}'.format(repo_name))
 
 
-for repo in g.get_user().get_repos():
-  if is_archived(repo):
-    print('Skipping repository {}, it is archived on github'.format(repo.name))
-  else:
-    if repo.name in ['bible-vue', 'bible-edge']:
-      print('Clonning repository {} locally'.format(repo.name))
-      clone(repo.name)
-      
+def is_repo_exists_on_aws(repo_name):
+    try:
+        codecommit_client.get_repository(repositoryName=repo_name)
+        return True
+    except Exception:
+        return False
+
+
+for repo in github_client.get_user().get_repos():
+    if repo.archived:
+        print('Skipping repository {}, it is archived on github'.format(repo.name))
+    else:
+        if repo.name in ['bible-vue']:
+            print('Cloning repository {} to local storage'.format(repo.name))
+            clone_repo(repo.name)
+
+            # Check if repo exists on AWS
+            if is_repo_exists_on_aws(repo.name):
+                print("mirror")
+                os.system('cd {} && git remote add sync ssh://git-codecommit.{}.amazonaws.com/v1/repos/{}'.format(
+                    repo.name,
+                    'eu-central-1',
+                    repo.name
+                ))
+                os.system('git push sync --mirror')
+            else:
+                print("create and mirror")
+
+            print('Deleting repository {} from local storage'.format(repo.name))
+            delete_repo_local(repo.name)
