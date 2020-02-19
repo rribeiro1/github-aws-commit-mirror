0ce64288163e41adfb953991ce6d5d56c0b66758
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-19 22:35:34 +0100

    wip

--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -1,9 +1,7 @@
 rm -Rf /home/circleci/.ssh/config
 
-cat ~/.ssh/id_rsa_ffeb190510d862a23307b06f1dd70fcf
-
 echo "Host !github.com *
 User APKAYMLLNMYWO5TS2QMO
 IdentityFile /home/circleci/.ssh/id_rsa_ffeb190510d862a23307b06f1dd70fcf" >> /home/circleci/.ssh/config
 
-ssh git-codecommit.us-east-2.amazonaws.com
+ssh -v APKAYMLLNMYWO5TS2QMO@git-codecommit.us-east-2.amazonaws.com
