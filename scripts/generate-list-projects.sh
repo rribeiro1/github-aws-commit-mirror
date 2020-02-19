6518278470679d254cc619436241b8e704164e22
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-19 23:14:35 +0100

    wip

--- scripts/generate-list-projects.sh
+++
@@ -1,1 +0,0 @@
-curl --silent -H "Authorization: $1" https://api.github.com/users/rribeiro1/repos?per_page=100 -q | grep "\"name\"" | awk -F': "' '{print $2}' | sed -e 's/",//g' >> repos.txt
--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -1,7 +1,7 @@
-# Removing default config for ssh
+# Remove default config for ssh
 rm -Rf /home/circleci/.ssh/config
 
-# Creating new config for AWS Code Commit 
+# Create ssh config for AWS Code Commit 
 echo "Host !github.com *
 User APKAYMLLNMYWO5TS2QMO
 IdentityFile /home/circleci/.ssh/id_rsa_ffeb190510d862a23307b06f1dd70fcf" >> /home/circleci/.ssh/config
