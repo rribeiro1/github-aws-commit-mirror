51fc3d531199e12b1e514a56b09c1d38c9e18e2d
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-19 22:54:08 +0100

    wip

--- src/github-aws-commit-mirror.py
+++ src/github-aws-commit-mirror.py
@@ -35,8 +35,7 @@ for repo in github_client.get_user().get_repos():
             # Check if repo exists on AWS
             if is_repo_exists_on_aws(repo.name):
                 os.system('ls -ltra')
-                os.system('cd {}'.format(repo.name))
-                os.system('ls -ltra')
+                os.system('cd {} && ls -ltra'.format(repo.name))
                 os.system('git remote add sync ssh://git-codecommit.eu-central-1.amazonaws.com/v1/repos/{}'.format(repo.name))
                 os.system('git push sync --mirror')
                 os.system('cd ..')
