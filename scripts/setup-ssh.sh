17dcd5fa10d32ce2ca6a3a05cec8d3a80e2cc239
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-20 00:06:34 +0100

    wip

--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -7,8 +7,8 @@ cat /home/circleci/.ssh/id_rsa_ffeb190510d862a23307b06f1dd70fcf
 
 # Add CodeCommit to SSH Configuration
 # see https://docs.aws.amazon.com/codecommit/latest/userguide/setting-up-without-cli.html#setting-up-without-cli-configure-client
- echo "Host !github.com *
- User APKAYMLLNMYWO5TS2QMO
+ echo "Host git-codecommit.*.amazonaws.com
+ User $1
  IdentityFile /home/circleci/.ssh/id_rsa" >> /home/circleci/.ssh/config
 
 ls -ltra ~/.ssh
@@ -17,4 +17,4 @@ ls -ltra ~/.ssh
 # cat /home/circleci/.ssh/config
 
 # Add CodeCommit Server as Known Host
-ssh-keyscan -H git-codecommit.$1.amazonaws.com >> ~/.ssh/known_hosts
+ssh-keyscan -H git-codecommit.$2.amazonaws.com >> ~/.ssh/known_hosts
