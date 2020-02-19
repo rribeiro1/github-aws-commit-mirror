4e02ec7b6778f6a7985eb59a8ee28dcf5d74261a
Rafael Ribeiro <rafael.pr10@gmail.com>
2020-02-19 22:12:46 +0100

    wip

--- scripts/setup-ssh.sh
+++ scripts/setup-ssh.sh
@@ -1,5 +1,6 @@
+rm -Rf ~/.ssh/config
 echo "
 Host !github.com *
   User AKIAYMLLNMYWP6P7RUB3
   IdentityFile /home/circleci/.ssh/id_rsa_ffeb190510d862a23307b06f1dd70fcf
-  " >> ~/.ssh/test
+  " >> ~/.ssh/config
